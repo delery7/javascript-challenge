85f9983e2fc4b80d85aaf0cbaf0cfa83345833ca
Dustin Elery <delery7@yahoo.com>
2021-03-18 21:13:42 -0400

    filter is appending to the table not showing only the filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -3,49 +3,47 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
-// console.log(tableData);
-
-// // // Step 1: Loop Through `data` and console.log each weather report object
-tableData.forEach(function(sightReport) {
-    // console.log(sightReport);
-  });
-
-//   / // Step 2:  Use d3 to append one table row `tr` for each weather report object
-tableData.forEach(function(sightReport) {
-    // console.log(sightReport);
-    var row = tbody.append("tr");
-  });
-
-  // // Step 3:  Use `Object.entries` to console.log each weather report value
-  tableData.forEach(function(sightReport) {
-    // console.log(sightReport);
-    var row = tbody.append("tr");
-  
-    Object.entries(sightReport).forEach(function([key, value]) {
-      console.log(key, value);
-    });
-  });
-
-  // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-tableData.forEach(function(sightReport) {
-    console.log(sightReport);
-    var row = tbody.append("tr");
-  
-    Object.entries(sightReport).forEach(function([key, value]) {
-    //   console.log(key, value);
-      // Append a cell to the row for each value
-      var cell = row.append("td");
-    });
-  });
 
  // // Step 5: Use d3 to update each cell's text with 
  tableData.forEach(function(sightReport) {
     // console.log(sightReport);
     var row = tbody.append("tr");
     Object.entries(sightReport).forEach(function([key, value]) {
-      console.log(key, value);
+      // console.log(key, value);
       // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", handleClick);
+
+// This function is triggered when the button is clicked
+function handleClick() {
+
+  d3.event.preventDefault();
+
+  var inputField = d3.select("#datetime");
+  var inputValue = inputField.property("value");
+
+  // console.log(inputValue);
+  // console.log(tableData);
+
+  var filteredData = tableData.filter(datetime => datetime.datetime === inputValue);
+
+
+  filteredData.forEach(function(sightReport) {
+    // console.log(sightReport);
+    var row = tbody.append("tr");
+    Object.entries(sightReport).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  // console.log(filteredData)
+  // console.log(d3.event.target);
+}
+
