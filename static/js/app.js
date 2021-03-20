7a118f7a69d39e3e3ad813b0c9e2d7b40c6551af
Dustin Elery <delery7@yahoo.com>
2021-03-20 13:58:46 -0400

    got the enter default to stop, ready for final submission

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,9 @@
-// from data.js
+// import data from data.js
 var tableData = data;
-
 
 var tbody = d3.select("tbody");
 
- // // Use d3 to update each cell's text with 
+ // // Use d3 to update each cell's text with and create the initial load chart on the page
  tableData.forEach(function(sightReport) {
     // console.log(sightReport);
     var row = tbody.append("tr");
@@ -15,33 +14,40 @@ var tbody = d3.select("tbody");
   });
 
 var button = d3.select("#filter-btn");
-var form = d3.select("#form");
+var form = d3.select("form");
 
+// activate the button and submit action
 button.on("click", handleClick);
 form.on("submit", handleClick);
 
-// This function is triggered when the button is clicked
+// This function is triggered when the button is clicked to apply the date filter
 function handleClick() {
 
+  // prevent hitting enter causing a page reload
   d3.event.preventDefault();
+
+  // creating the fields for the date input for filtering
 
   var inputField = d3.select("#datetime");
   var inputValue = inputField.property("value");
-
-  // console.log(inputValue);
-
+  
+  // getting the filtered data
   var filteredData = tableData.filter(datetime => datetime.datetime === inputValue);
   
+  // getting the body tag where the table code exists
   var tbody = d3.select("tbody");
   
+  // clears the initial build table
   tbody.html("");
- 
+  
+  // rewrites the table with the filtered results
   filteredData.forEach(function(sightReport) {
     var row = tbody.append("tr");
     Object.entries(sightReport).forEach(function([key, value]) {
       var cell = row.append("td");
       cell.text(value);
     });
+    d3.event.preventDefault();
   });
 
 }
