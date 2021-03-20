726fa6ef936d7f610a08049d2f618a6a423fdc44
Dustin Elery <delery7@yahoo.com>
2021-03-20 13:12:34 -0400

    Read me and folders cleaned up

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
 var tbody = d3.select("tbody");
 
- // // Step 5: Use d3 to update each cell's text with 
+ // // Use d3 to update each cell's text with 
  tableData.forEach(function(sightReport) {
     // console.log(sightReport);
     var row = tbody.append("tr");
     Object.entries(sightReport).forEach(function([key, value]) {
-      // console.log(key, value);
-      // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
@@ -20,6 +18,7 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 button.on("click", handleClick);
+form.on("submit", handleClick);
 
 // This function is triggered when the button is clicked
 function handleClick() {
@@ -30,20 +29,20 @@ function handleClick() {
   var inputValue = inputField.property("value");
 
   // console.log(inputValue);
-  // console.log(tableData);
 
   var filteredData = tableData.filter(datetime => datetime.datetime === inputValue);
-
-
+  
+  var tbody = d3.select("tbody");
+  
+  tbody.html("");
+ 
   filteredData.forEach(function(sightReport) {
-    // console.log(sightReport);
     var row = tbody.append("tr");
     Object.entries(sightReport).forEach(function([key, value]) {
       var cell = row.append("td");
       cell.text(value);
     });
   });
-  // console.log(filteredData)
-  // console.log(d3.event.target);
+
 }
 
